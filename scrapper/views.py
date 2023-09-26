97cd8f47441c3917aeab8af9ab3d6882dae5958c
Otero <oteronicolas3@gmail.com>
2023-09-26 23:35:50 +1000

    Hasta Manana

--- scrapper/views.py
+++ scrapper/views.py
@@ -18,75 +18,103 @@ paramount_plus = "paramount_plus"
 apple_tv_us = "apple_tv_us"
 
 def landing(request):
-    return render(request, 'landing_page.html')
+    contexto = {'peliculas': scrap('https://www.rottentomatoes.com/browse/movies_in_theaters/', 'pelicula'), 'series': scrap('https://www.rottentomatoes.com/browse/tv_series_browse/', 'serie')}
+    print(contexto)
+    return render(request, 'landing_page.html', {'contexto': contexto})
+
+'''
+contexto = {
+    'peliculas': pelicula = {
+                        'Nombre_pelicula': nombre_pelicula,
+                        'Fecha_streaming': fecha_streaming,
+                        'Imagen': imagen,
+                        'Criticsscore': criticsscore,
+                        'Audiencescore': audiencescore
+                        },
+    'series': serie = {
+                        'Nombre_serie': nombre_serie,
+                        'Fecha_streaming': fecha_streaming,
+                        'Imagen': valor_imagen,
+                        'Criticsscore': criticsscore,
+                        'Audiencescore': audiencescore
+                        },
+}
+'''
 
 def tipo_view(request, tipo):
     if tipo == 'pelicula':
-        return render(request, 'peliculas.html', scrap('https://www.rottentomatoes.com/browse/movies_at_home/?page=5'))
+        print(scrap('https://www.rottentomatoes.com/browse/movies_at_home/?page=5', 'pelicula'))
+        return render(request, 'peliculas.html', scrap('https://www.rottentomatoes.com/browse/movies_at_home/?page=5', 'pelicula'))
     elif tipo == 'serie':
-        return render(request, 'series.html', scrap('https://www.rottentomatoes.com/browse/tv_series_browse/?page=5'))
+        print(scrap('https://www.rottentomatoes.com/browse/movies_at_home/?page=5', 'serie'))
+        return render(request, 'series.html', scrap('https://www.rottentomatoes.com/browse/tv_series_browse/?page=5', 'serie'))
 
 def stream_view(request, tipo, stream):
     if tipo == 'pelicula':
-        return render(request, 'peliculas.html', scrap(f"https://www.rottentomatoes.com/browse/movies_at_home/affiliates:{str(stream)}?page=5"))
+        return render(request, 'peliculas.html', scrap(f"https://www.rottentomatoes.com/browse/movies_at_home/affiliates:{str(stream)}?page=5", 'pelicula'))
     elif tipo == 'serie':
-        return render(request, 'series.html', scrap(f"https://www.rottentomatoes.com/browse/tv_series_browse/affiliates:{str(stream)}?page=5"))
+        return render(request, 'series.html', scrap(f"https://www.rottentomatoes.com/browse/tv_series_browse/affiliates:{str(stream)}?page=5", 'serie'))
 
-def scrap(url):
+def scrap(url, tipo):
     response = requests.get(url)
     html_content = response.content
 
     soup = BeautifulSoup(html_content, "html.parser")
 
-    divs = soup.find_all('div', class_='js-tile-link')
-    links = soup.find_all('a', class_='js-tile-link')
+    #divs = soup.find_all('div', class_='js-tile-link')
+    #links = soup.find_all('a', class_='js-tile-link')
+    elementos = soup.select('div.js-tile-link, a.js-tile-link')
 
     datos_peliculas = []
-    for div, link in zip(divs, links):
-        # Extrae los datos que necesitas de cada etiqueta
-        nombre_pelicula = div.find('span', class_='p--small').get_text(strip=True)
-        fecha_streaming_tag = div.find('span', class_='smaller')
-        if fecha_streaming_tag:
-            fecha_streaming = fecha_streaming_tag.get_text(strip=True)
-        else:
-            # Si no se encuentra, asigna un valor por defecto
-            fecha_streaming = '-'
-        imagen = div.find('img')['src']
-        criticsscore = div.find('score-pairs')['criticsscore']
-        audiencescore = div.find('score-pairs')['audiencescore']
+    datos_series = []
+    if tipo == 'pelicula':
+        for elemento in elementos:
+            # Extrae los datos que necesitas de cada etiqueta
+            nombre_pelicula = elemento.find('span', class_='p--small').get_text(strip=True)
+            fecha_streaming_tag = elemento.find('span', class_='smaller')
+            if fecha_streaming_tag:
+                fecha_streaming = fecha_streaming_tag.get_text(strip=True)
+            else:
+                # Si no se encuentra, asigna un valor por defecto
+                fecha_streaming = '-'
+            imagen = elemento.find('img')['src']
+            criticsscore = elemento.find('score-pairs')['criticsscore']
+            audiencescore = elemento.find('score-pairs')['audiencescore']
 
-        pelicula = {
-            'Nombre_pelicula': nombre_pelicula,
-            'Fecha_streaming': fecha_streaming,
-            'Imagen': imagen,
-            'Criticsscore': criticsscore,
-            'Audiencescore': audiencescore
-        }
+            pelicula = {
+                'Nombre_pelicula': nombre_pelicula,
+                'Fecha_streaming': fecha_streaming,
+                'Imagen': imagen,
+                'Criticsscore': criticsscore,
+                'Audiencescore': audiencescore
+            }
 
-        # Agrega el diccionario a la lista de datos de películas
-        datos_peliculas.append(pelicula)
+            # Agrega el diccionario a la lista de datos de películas
+            datos_peliculas.append(pelicula)
+        return {'peliculas': datos_peliculas}
+        
+    elif tipo == 'serie':
+        for elemento in elementos:
+            # Extrae los datos que necesitas de cada etiqueta
+            nombre_serie = elemento.find('span', class_='p--small').get_text(strip=True)
+            fecha_streaming_tag = elemento.find('span', class_='smaller')
+            if fecha_streaming_tag:
+                fecha_streaming = fecha_streaming_tag.get_text(strip=True)
+            else:
+                # Si no se encuentra, asigna un valor por defecto
+                fecha_streaming = '-'
+            imagen = elemento.find('img')['src']
+            criticsscore = elemento.find('score-pairs')['criticsscore']
+            audiencescore = elemento.find('score-pairs')['audiencescore']
 
-        nombre_pelicula = link.find('span', class_='p--small').get_text(strip=True)
-        fecha_streaming_tag = div.find('span', class_='smaller')
-        if fecha_streaming_tag:
-            fecha_streaming = fecha_streaming_tag.get_text(strip=True)
-        else:
-            # Si no se encuentra, asigna un valor por defecto
-            fecha_streaming = '-'
-        imagen = link.find('img')['src']
-        criticsscore = link.find('score-pairs')['criticsscore']
-        audiencescore = link.find('score-pairs')['audiencescore']
+            serie = {
+                'Nombre_serie': nombre_serie,
+                'Fecha_streaming': fecha_streaming,
+                'Imagen': imagen,
+                'Criticsscore': criticsscore,
+                'Audiencescore': audiencescore
+            }
 
-        pelicula = {
-            'Nombre_pelicula': nombre_pelicula,
-            'Fecha_streaming': fecha_streaming,
-            'Imagen': imagen,
-            'Criticsscore': criticsscore,
-            'Audiencescore': audiencescore
-        }
-    
-        # Agrega el diccionario a la lista de datos de películas
-        datos_peliculas.append(pelicula)
-    
-    print(datos_peliculas)
-    return ({'pelis': datos_peliculas})
+            # Agrega el diccionario a la lista de datos de películas
+            datos_series.append(serie)
+        return {'series': datos_series}
