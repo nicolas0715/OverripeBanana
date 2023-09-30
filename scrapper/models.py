b4381734e6feca22bbc0853bcdca973d3509fdb8
Otero <oteronicolas3@gmail.com>
2023-10-01 03:06:56 +1100

    Prueba1

--- scrapper/models.py
+++ scrapper/models.py
@@ -28,7 +28,7 @@ class User(models.Model):
 
 class Pelicula(models.Model):
     nombre_pelicula = models.CharField(max_length=255, verbose_name='Nombre_Pelicula')
-    fecha_streaming = models.DateField(verbose_name='Fecha_Streaming')
+    fecha_streaming = models.DateField(blank=True, verbose_name='Fecha_Streaming')
     imagen = models.URLField(verbose_name='Imagen')
     critics_score = models.IntegerField(default=0, verbose_name='Critics Score')
     audience_score = models.IntegerField(default=0, verbose_name='Audience Score')
@@ -37,7 +37,7 @@ class Pelicula(models.Model):
 
 class Serie(models.Model):
     nombre_serie = models.CharField(max_length=255, verbose_name='Nombre_Serie')
-    ultimo_capitulo = models.DateField(verbose_name='Ultimo_Capitulo')
+    ultimo_capitulo = models.DateField(blank=True, verbose_name='Ultimo_Capitulo')
     imagen = models.URLField(verbose_name='Imagen_Serie')
     critics_score = models.IntegerField(default=0, verbose_name='Critics Score')
     audience_score = models.IntegerField(default=0, verbose_name='Audience Score')
